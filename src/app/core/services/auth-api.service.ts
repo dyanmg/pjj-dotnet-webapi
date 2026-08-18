d0274b78c06194295b4ec505a436ccf306aab5d6
dyanmg <gufron.dian@gmail.com>
2026-08-18 08:59:32 +0700

    add login page

--- src/app/core/services/auth-api.service.ts
+++ src/app/core/services/auth-api.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { LoginRequest, LoginResponse } from '../models/auth.model';
 
 @Service()
 export class AuthApiService {
-    private readonly _baseUrl = 'http://localhost:5000/api/Account';
+    private readonly _baseUrl = 'http://localhost:5000';
     private readonly _httpClient = inject(HttpClient);
 
     register(data: any): Observable<any> {
-        return this._httpClient.post(`${this._baseUrl}/register-pegawai`, data)
+        return this._httpClient.post(`${this._baseUrl}/api/Account/register-pegawai`, data)
             .pipe(
                 map((response: any) => response.data)
             );
+    }
+
+    login(request: LoginRequest) {
+        return this._httpClient.post<LoginResponse>(`${this._baseUrl}/login`, request);
     }
 }
--- src/app/features/auth/pages/login/login.page.ts
+++ src/app/features/auth/pages/login/login.page.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AuthApiService } from '../../../../core/services/auth-api.service';
+import { LoginRequest } from '../../../../core/models/auth.model';
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    ReactiveFormsModule,
+    MatInput,
+    MatButtonModule
+  ],
   templateUrl: './login.page.html',
   styleUrl: './login.page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+  private readonly _formBuilder: FormBuilder = inject(FormBuilder);
+  private readonly _authService: AuthApiService = inject(AuthApiService);
+
+  form = this._formBuilder.group({
+    email: [''],
+    password: [''],
+  });
+
+  onSubmit(): void {
+    this._authService.login(this.form.value as LoginRequest)
+      .subscribe((result: any) => {
+        alert('Login berhasil!');
+      });
+  }
+}
